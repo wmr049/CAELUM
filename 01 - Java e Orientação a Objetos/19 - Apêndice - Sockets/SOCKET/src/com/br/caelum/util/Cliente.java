bf50a75000341dd935f846eae85b6c76df54ee81
wmr049 <wmr049@gmail.com>
2015-07-19 18:27:08 -0300

    Commit curso Java 19 - Apêndice - Sockets

---
+++ 01 - Java e Orientação a Objetos/19 - Apêndice - Sockets/SOCKET/src/com/br/caelum/util/Cliente.java
@@ -0,0 +1,45 @@
+package com.br.caelum.util;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Cliente {
+
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
+		new Cliente("127.0.0.1", 12345).executa();
+	}
+
+	private String host;
+	private int porta;
+
+	public Cliente(String host, int porta) {
+		this.host = host;
+		this.porta = porta;
+	}
+
+	private void executa() throws UnknownHostException, IOException {
+		Socket cliente = new Socket(this.host, this.porta);
+		System.out.println("O cliente se conectou ao servidor!");
+		
+		// thread para receber mensagens do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream());
+		new Thread(r).start();
+		
+		// lê msgs do teclado e manda pro servidor
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		
+		while (teclado.hasNextLine()) {
+			saida.println(teclado.nextLine());
+		}
+		saida.close();
+		teclado.close();
+		cliente.close();
+
+	}
+
+}
---
+++ 01 - Java e Orientação a Objetos/19 - Apêndice - Sockets/SOCKET/src/com/br/caelum/util/TratamentoClass.java
@@ -0,0 +1,11 @@
+package com.br.caelum.util;
+
+import java.net.Socket;
+
+public class TratamentoClass {
+
+	public TratamentoClass(Socket cliente) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
