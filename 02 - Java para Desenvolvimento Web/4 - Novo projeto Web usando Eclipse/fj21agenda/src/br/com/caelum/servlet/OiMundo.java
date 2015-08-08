e0f89260ac8d2e0972df765d3f6a2c479c5a8a9c
wmr049 <wmr049@gmail.com>
2015-08-08 05:39:54 -0300

    Curso Java Caelum FJ21 - 4 - Novo projeto Web usando Eclipse

---
+++ 02 - Java para Desenvolvimento Web/4 - Novo projeto Web usando Eclipse/fj21agenda/src/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,17 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OiMundo extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(request, response);
+	}
+}
