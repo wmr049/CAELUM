2b8115496dbca623fbd1fa62e8c86d41b66793b4
wmr049 <wmr049@gmail.com>
2015-08-08 05:54:05 -0300

    Curso Java Caelum FJ21 - 4 - Novo projeto Web usando Eclipse - 5.5 EXERCÍCIOS : PRIMEIRA SERVLET

--- 02 - Java para Desenvolvimento Web/4 - Novo projeto Web usando Eclipse/fj21agenda/src/br/com/caelum/servlet/OiMundo.java
+++ 02 - Java para Desenvolvimento Web/4 - Novo projeto Web usando Eclipse/fj21agenda/src/br/com/caelum/servlet/OiMundo.java
@@ -1,6 +1,7 @@
 package br.com.caelum.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,9 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 public class OiMundo extends HttpServlet {
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(request, response);
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Oi mundo Servlet!</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 }
