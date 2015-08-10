31b9be22394a117326a8e9b2c09d44484de6e792
wmr049 <wmr049@gmail.com>
2015-08-10 06:22:06 -0300

    Curso Java Caelum FJ21 - 5 - Servlets

--- 02 - Java para Desenvolvimento Web/2.1 - Exercicio 1/src/br/com/caelum/jdbc/teste/JDBCInsere.java
+++ 02 - Java para Desenvolvimento Web/2.1 - Exercicio 1/src/br/com/caelum/jdbc/teste/JDBCInsere.java
@@ -17,8 +17,8 @@ public class JDBCInsere {
 		// pronto para gravar
 		Contato contato = new Contato();
 		contato.setNome("Milton");
-		contato.setEmail("wmr049@caelum.com.br");
-		contato.setEndereco("R. Vergueiro 1 cj87");
+		contato.setEmail("wmr049@gmail.com");
+		contato.setEndereco("R. 1 ");
 		contato.setDataNascimento(Calendar.getInstance());
 		// grave nessa conexão!!!
 		ContatoDao bd = new ContatoDao();
