8f2026791f92aee6012dc768d28ce02fe3a45b7c
wmr049 <wmr049@gmail.com>
2015-08-14 06:44:31 -0300

    delete

--- 02 - Java para Desenvolvimento Web/2.1 - Exercicio 1/src/br/com/caelum/jdbc/dao/ContatoDao.java
+++ 02 - Java para Desenvolvimento Web/2.1 - Exercicio 1/src/br/com/caelum/jdbc/dao/ContatoDao.java
@@ -3,7 +3,11 @@ package br.com.caelum.jdbc.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import br.com.caelum.jdbc.ConnectionFactory;
 import br.com.caelum.jdbc.modelo.Contato;
@@ -13,6 +17,34 @@ public class ContatoDao {
 
 	public ContatoDao() {
 		this.connection = new ConnectionFactory().getConnection();
+	}
+
+	public List<Contato> getLista() {
+		try {
+			List<Contato> contatos = new ArrayList<Contato>();
+			PreparedStatement stmt = this.connection
+					.prepareStatement("select * from contatos");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				// criando o objeto Contato
+				Contato contato = new Contato();
+				contato.setId(rs.getLong("id"));
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+				// montando a data através do Calendar
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+				// adicionando o objeto à lista
+				contatos.add(contato);
+			}
+			rs.close();
+			stmt.close();
+			return contatos;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public void adiciona(Contato contato) throws SQLException {
@@ -32,8 +64,39 @@ public class ContatoDao {
 			stmt.close();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		}finally {
+		} finally {
 			connection.close();
 		}
 	}
+
+	public void altera(Contato contato) {
+		String sql = "update contatos set nome=?, email=?, endereco=?,"
+				+ "dataNascimento=? where id=?";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento()
+					.getTimeInMillis()));
+			stmt.setLong(5, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void remove(Contato contato) {
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete"
+					+ "from contatos where id=?");
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
